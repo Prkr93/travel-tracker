f6a77a5513cb958aef5b7b2754438af6c071e9dd
Dillon Parker <prkr93@gmail.com>
2022-03-06 12:14:43 -0700

    Add test data for destinations, traveler, and trips

---
+++ src/data/traveler-test-data.js
@@ -0,0 +1,16 @@
+const travelerTestData = [
+  {
+    id: 1,
+    name: "Ham Leadbeater",
+    travelerType: "relaxer"
+  },
+  {
+    id: 2,
+    name: "Rachael Vaughten",
+    travelerType: "thrill-seeker"
+  }
+]
+
+
+
+export { travelerTestData };
---
+++ src/data/trip-test-data.js
@@ -0,0 +1,44 @@
+const tripTestData = [
+  {
+    id: 1,
+    userID: 1,
+    destinationID: 1,
+    travelers: 1,
+    date: "2022/09/16",
+    duration: 8,
+    status: "pending",
+    suggestedActivities: [ ]
+  },
+  {
+    id: 2,
+    userID: 2,
+    destinationID: 2,
+    travelers: 5,
+    date: "2022/10/04",
+    duration: 18,
+    status: "approved",
+    suggestedActivities: [ ]
+  },
+  {
+    id: 3,
+    userID: 1,
+    destinationID: 3,
+    travelers: 4,
+    date: "2022/01/22",
+    duration: 17,
+    status: "approved",
+    suggestedActivities: [ ]
+  },
+  {
+    id: 4,
+    userID: 2,
+    destinationID: 2,
+    travelers: 2,
+    date: "2022/02/25",
+    duration: 10,
+    status: "approved",
+    suggestedActivities: [ ]
+  },
+]
+
+export {tripTestData};
